04aa0bf7c3d7ce27f94bb3a35cf133a25ab6ba07
nandad301 <82rakka@gmail.com>
2025-07-01 09:51:36 -0700

    Update index_test.php

--- tests/index_test.php
+++ tests/index_test.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/../src/functions.php';
+require_once __DIR__ . '/../src/function.php';
 
 function assertEqual($expected, $actual, $testName) {
     if ($expected === $actual) {
